cd0649ac183c58903fbc542f086953d41a4d453d
airtoxin <airtoxin@icloud.com>
2018-10-14 17:08:31 +0900

    Export gif

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate gif;
+
+use gif::{Frame, Encoder, Repeat, SetParameter};
+use std::fs::File;
+use std::borrow::Cow;
+
 fn main() {
-    println!("Hello, world!");
+    let color_map = &[0xFF, 0xFF, 0xFF, 0, 0, 0];
+    let (width, height) = (6, 6);
+    let beacon_states = [[
+        0, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 0,
+        0, 0, 0, 1, 1, 0,
+        0, 0, 0, 0, 0, 0,
+    ], [
+        0, 0, 0, 0, 0, 0,
+        0, 1, 1, 0, 0, 0,
+        0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0,
+        0, 0, 0, 1, 1, 0,
+        0, 0, 0, 0, 0, 0,
+    ], [
+        0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 1, 0,
+        0, 1, 0, 0, 1, 0,
+        0, 1, 0, 0, 1, 0,
+        0, 1, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0,
+    ],];
+    let mut image = File::create("target/beacon.gif").unwrap();
+    let mut encoder = Encoder::new(&mut image, width, height, color_map).unwrap();
+    encoder.set(Repeat::Infinite).unwrap();
+    for state in &beacon_states {
+        let mut frame = Frame::default();
+        frame.width = width;
+        frame.height = height;
+        frame.buffer = Cow::Borrowed(&*state);
+        encoder.write_frame(&frame).unwrap();
+    }
 }
